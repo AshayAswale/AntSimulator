7b6ee7fc75521ca1715dc655e63809f43292936d
Khai Yi Chin <khaiyichin@gmail.com>
2022-11-12 23:56:23 -0600

    Implemented feature to read XML configuration file, removing the need to modify source code for experiment configuration.

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include "colony.hpp"
 #include "config.hpp"
 #include "display_manager.hpp"
+#include "tinyxml2.h"
 
 #include <stdio.h>      // for sprintf()
 
@@ -16,81 +17,151 @@
 ************************ CHANGE THESE PARAMETERS FOR TRIALS ************************
 ****************************************************************************************/
 /*
-* @param DISPLAY_GUI:: Do you want GUI? If set to true, you can see the simulation
-* @param SIMULATION_STEPS:: Number of steps of simulation (Will not be in effect for GUI)
-* @param SIMULATION_ITERATIONS:: Run the same configured iteration these number of times
-* @param malicious_fraction:: Probability of an ant being malicious (fraction of ants being malicious)
-* @param malicious_timer_wait:: Delay after which the attack is launched
-* @param malicious_ants_focus::  Should the attack be focused towards food
-* @param ant_tracing_pattern::   Should malicious ants trace food pheromone or roam randomly
-* @param counter_pheromone:: Will the ants secret counter pheromone?
-* @param hell_phermn_intensity_multiplier:: multiplier for the intensity of TO_HELL pheromone
+* @param sim_config.gui_display:: Do you want GUI? If set to true, you can see the simulation
+* @param sim_config.sim_steps:: Number of steps of simulation (Will not be in effect for GUI)
+* @param sim_config.sim_iterations:: Run the same configured iteration these number of times
+* @param sim_config.malicious_fraction:: Probability of an ant being malicious (fraction of ants being malicious)
+* @param sim_config.malicious_timer_wait:: Delay after which the attack is launched
+* @param sim_config.malicious_focus::  Should the attack be focused towards food
+* @param sim_config.tracing_pattern::   Should malicious ants trace food pheromone or roam randomly
+* @param sim_config.patience_activation:: Will the ants secret counter pheromone?
+* @param sim_config.malicious_intensity_mult:: multiplier for the intensity of TO_HELL pheromone
 */
-const bool DISPLAY_GUI = true;
-const int SIMULATION_STEPS = 50000;		// Only used in the data recording, NOT IN GUI
-const int SIMULATION_ITERATIONS = 20;
-float malicious_fraction = std::pow(2,-5);
-int dilusion_max = 500;
-int malicious_timer_wait = 100;	
-bool malicious_ants_focus = false;
-AntTracingPattern ant_tracing_pattern = AntTracingPattern::FOOD;
-bool counter_pheromone = true;
-float hell_phermn_intensity_multiplier = 1;
-float hell_phermn_evpr_multi = 10.0;
-float cntr_phermn_evpr_multi = 1.0;
-int dilusion_increment = 500;
+// const bool sim_config.gui_display = true;
+// const int sim_config.sim_steps = 50000;		// Only used in the data recording, NOT IN GUI
+// const int sim_config.sim_iterations = 100;
+// float sim_config.malicious_fraction = std::pow(2,-2);
+// int sim_config.patience_max_val = 500;
+// int sim_config.malicious_timer_wait = 100;
+// bool sim_config.malicious_focus = true;
+// AntTracingPattern sim_config.tracing_pattern = AntTracingPattern::FOOD;
+// bool sim_config.patience_activation = true;
+// float sim_config.malicious_intensity_mult = 1;
+// float sim_config.malicious_evaporation_mult = 10.0;
+// float sim_config.patience_evaporation_mult = 1.0;
+// int sim_config.patience_refill_inc = 500;
+
+struct SimulationConfiguration
+{
+	SimulationConfiguration() {};
+
+	bool gui_display = false;
+
+	bool gui_fullscreen = false;
+
+	int sim_steps = 50000;
+
+	int sim_iterations = 100;
+
+	int reg_ant_number = 1024;
+
+	bool patience_activation = false;
+
+	int patience_max_val = 500;
+
+	int patience_refill_inc = 500;
+
+	float patience_evaporation_mult = 1.0;
+
+	bool malicious_focus = false;
+
+	int malicious_timer_wait = 100;
+
+	float malicious_fraction = 1e-2;
+
+	float malicious_intensity_mult = 1;
+
+	float malicious_evaporation_mult = 10.0;
+
+	AntTracingPattern tracing_pattern = AntTracingPattern::FOOD; // does this ever change?
+
+	std::string csv_prefix;
+};
+
+SimulationConfiguration sim_config; // define global variable
 
 std::string getExperimentSpecificName(int iteration)
 {
-std::string DISPLAY_GUI_string = "_DISPLAY_GUI-" + std::to_string(DISPLAY_GUI);
-std::string SIMULATION_STEPS_string = "_SIM_STEPS-" + std::to_string(SIMULATION_STEPS);
-std::string SIMULATION_ITERATIONS_string = "_SIM_ITERS-" + std::to_string(SIMULATION_ITERATIONS);
-std::string malicious_fraction_string = "_mal_frac-" + std::to_string(malicious_fraction);
-std::string malicious_timer_wait_string = "_mal_delay-" + std::to_string(malicious_timer_wait);
-std::string malicious_ants_focus_string = "_mal_ants_focus-" + std::to_string(malicious_ants_focus);
-std::string ant_tracing_pattern_string = "_ant_tracing-" + std::to_string(ant_tracing_pattern);
-std::string counter_pheromone_string = "_ctr_pherm-" + std::to_string(counter_pheromone);
-std::string hell_phermn_intensity_multiplier_string = "_hell_phermn_intens-" + std::to_string(hell_phermn_intensity_multiplier);
-std::string hell_phermn_evpr_multi_string = "_hell_phermn_evpr-" + std::to_string(hell_phermn_evpr_multi);
-std::string dilusion_max_string = "_dil_max-" + std::to_string(dilusion_max);
-std::string dilusion_increment_string = "_dil_incr-" + std::to_string(dilusion_increment);
-std::string iteration_string = "_iter-" + std::to_string(iteration);
+	std::string DISPLAY_GUI_string = "_DISPLAY_GUI-" + std::to_string(sim_config.gui_display);
+	std::string SIMULATION_STEPS_string = "_SIM_STEPS-" + std::to_string(sim_config.sim_steps);
+	std::string SIMULATION_ITERATIONS_string = "_SIM_ITERS-" + std::to_string(sim_config.sim_iterations);
+	std::string malicious_fraction_string = "_mal_frac-" + std::to_string(sim_config.malicious_fraction);
+	std::string malicious_timer_wait_string = "_mal_delay-" + std::to_string(sim_config.malicious_timer_wait);
+	std::string malicious_ants_focus_string = "_mal_ants_focus-" + std::to_string(sim_config.malicious_focus);
+	std::string ant_tracing_pattern_string = "_ant_tracing-" + std::to_string(sim_config.tracing_pattern);
+	std::string counter_pheromone_string = "_ctr_pherm-" + std::to_string(sim_config.patience_activation);
+	std::string hell_phermn_intensity_multiplier_string = "_hell_phermn_intens-" + std::to_string(sim_config.malicious_intensity_mult);
+	std::string hell_phermn_evpr_multi_string = "_hell_phermn_evpr-" + std::to_string(sim_config.malicious_evaporation_mult);
+	std::string dilusion_max_string = "_dil_max-" + std::to_string(sim_config.patience_max_val);
+	std::string dilusion_increment_string = "_dil_incr-" + std::to_string(sim_config.patience_refill_inc);
+	std::string iteration_string = "_iter-" + std::to_string(iteration);
 
-return SIMULATION_STEPS_string
-		+ SIMULATION_ITERATIONS_string
-		+ malicious_fraction_string
-		+ malicious_timer_wait_string
-		+ malicious_ants_focus_string
-		+ ant_tracing_pattern_string
-		+ counter_pheromone_string
-		+ hell_phermn_intensity_multiplier_string
-		+ hell_phermn_evpr_multi_string
-		+ dilusion_max_string
-		+ dilusion_increment_string
-		+ iteration_string;
+	return SIMULATION_STEPS_string
+			+ SIMULATION_ITERATIONS_string
+			+ malicious_fraction_string
+			+ malicious_timer_wait_string
+			+ malicious_ants_focus_string
+			+ ant_tracing_pattern_string
+			+ counter_pheromone_string
+			+ hell_phermn_intensity_multiplier_string
+			+ hell_phermn_evpr_multi_string
+			+ dilusion_max_string
+			+ dilusion_increment_string
+			+ iteration_string;
 }
 
 void loadUserConf()
-{	
-	std::ifstream conf_file("conf.txt");
-	if (conf_file) {
-		conf_file >> malicious_fraction;
-		conf_file >> malicious_timer_wait;
-		conf_file >> Conf::ANTS_COUNT;
-	}
-	else {
-		// malicious_fraction = 0.25;
-		// malicious_timer_wait = 100;
-		std::cout << "Couldn't find 'conf.txt', loading default" << std::endl;
+{
+	tinyxml2::XMLDocument doc;
+
+	// Populate simulation parameters only if file exists, otherwise use default values
+	if (doc.LoadFile("config.xml") == 0)
+	{
+		tinyxml2::XMLElement* root = doc.FirstChildElement("antsim");
+
+		// Get GUI settings
+		tinyxml2::XMLElement* gui_element = root->FirstChildElement("gui");
+		sim_config.gui_display = gui_element->FirstChildElement("activate")->BoolAttribute("bool");
+		sim_config.gui_fullscreen = gui_element->FirstChildElement("fullscreen")->BoolAttribute("bool");
+
+		// Get simulation settings
+		tinyxml2::XMLElement* sim_element = root->FirstChildElement("simulation");
+		sim_config.sim_steps = sim_element->FirstChildElement("steps")->IntAttribute("int");
+		sim_config.sim_iterations = sim_element->FirstChildElement("iterations")->IntAttribute("int");
+
+		// Get regular ant settings
+		tinyxml2::XMLElement* reg_ants_element = root->FirstChildElement("regular_ants");
+		sim_config.reg_ant_number = reg_ants_element->FirstChildElement("number")->IntAttribute("int");
+		Conf::ANTS_COUNT = sim_config.reg_ant_number; // @todo: this shouldn't really be done this way, but the config file has been hardcoded
+
+		// Get patience/cautionary settings
+		tinyxml2::XMLElement* patience_element = root->FirstChildElement("patience");
+		sim_config.patience_activation = patience_element->FirstChildElement("activate")->BoolAttribute("bool");
+		sim_config.patience_refill_inc = patience_element->FirstChildElement("refill_increment")->IntAttribute("int");
+		sim_config.patience_max_val = patience_element->FirstChildElement("max")->IntAttribute("int");
+		sim_config.patience_evaporation_mult = patience_element->FirstChildElement("pheromone_evaporation_multiplier")->FloatAttribute("float");
+
+		// Get malicious ants settings
+		tinyxml2::XMLElement* malicious_element = root->FirstChildElement("malicious_ants");
+		sim_config.malicious_fraction = malicious_element->FirstChildElement("fraction")->FloatAttribute("float");
+		sim_config.malicious_focus = malicious_element->FirstChildElement("focus")->BoolAttribute("bool");
+		sim_config.malicious_timer_wait = malicious_element->FirstChildElement("timer")->IntAttribute("int");
+		sim_config.malicious_intensity_mult = malicious_element->FirstChildElement("pheromone_intensity_multiplier")->FloatAttribute("float");
+		sim_config.malicious_evaporation_mult = malicious_element->FirstChildElement("pheromone_evaporation_multiplier")->FloatAttribute("float");
+
+		// Get CSV filepath
+		const char *temp_str;
+		root->FirstChildElement("csv_output")->QueryStringAttribute("prefix", &temp_str);
+		sim_config.csv_prefix = std::string(temp_str);
 	}
 }
 
 void setStaticVariables()
 {
-	WorldCell::setHellPhermnEvprMulti(hell_phermn_evpr_multi);
+	WorldCell::setHellPhermnEvprMulti(sim_config.malicious_evaporation_mult);
 	Ant::resetFoodBitsCounters();
-	Ant::setDilusionMax(dilusion_max);
-	Ant::setDilusionIncrement(dilusion_increment);
+	Ant::setDilusionMax(sim_config.patience_max_val);
+	Ant::setDilusionIncrement(sim_config.patience_refill_inc);
 }
 
 void initWorld(World& world, Colony& colony)
@@ -132,8 +203,8 @@ void oneExperiment(int i)
 	std::ofstream myfile;
 	const static float dt = 0.016f;
 	const static int datapoints_to_record = 100;
-	static int skip_steps = SIMULATION_STEPS/datapoints_to_record;
-	static std::string file_name_prefix = "../data_exp-counters-multi-iteration/AntSimData";
+	static int skip_steps = sim_config.sim_steps/datapoints_to_record;
+	static std::string file_name_prefix = sim_config.csv_prefix;
 	static int x = 0;
 	
 	myfile.open(file_name_prefix+getExperimentSpecificName(i)+".csv");
@@ -145,12 +216,17 @@ void oneExperiment(int i)
 	
 	setStaticVariables();
 	World world(Conf::WORLD_WIDTH, Conf::WORLD_HEIGHT);
-	Colony colony(Conf::COLONY_POSITION.x, Conf::COLONY_POSITION.y, Conf::ANTS_COUNT, 
-	malicious_fraction, malicious_timer_wait, malicious_ants_focus, ant_tracing_pattern, 
-	counter_pheromone, hell_phermn_intensity_multiplier);
+	Colony colony(Conf::COLONY_POSITION.x,
+				  Conf::COLONY_POSITION.y, Conf::ANTS_COUNT,
+				  sim_config.malicious_fraction,
+				  sim_config.malicious_timer_wait,
+				  sim_config.malicious_focus,
+				  sim_config.tracing_pattern,
+				  sim_config.patience_activation,
+				  sim_config.malicious_intensity_mult);
 	initWorld(world, colony);	
 
-	for(int j = 0; j<SIMULATION_STEPS; j++)
+	for(int j = 0; j<sim_config.sim_steps; j++)
 	{
 		updateColony(world, colony);
 		if(j%skip_steps == 0)
@@ -171,18 +247,18 @@ void oneExperiment(int i)
 void simulateAnts()
 {
 	/**
-	 * @brief This loop will start a new colony and run the sim for SIMULATION_STEPS number of steps
+	 * @brief This loop will start a new colony and run the sim for sim_config.sim_steps number of steps
 	 */
 	std::vector<int> dilusion_max_set = {50, 100, 250, 500, 750, 1000};
 	std::vector<int> dilusion_increment_power = {1, 2, 5, 10, 50, 100, 1000};
-	for(int i = 0; i<SIMULATION_ITERATIONS; i++)
+	for(int i = 0; i<sim_config.sim_iterations; i++)
 	{
 		for(int dil_max = 0; dil_max<dilusion_max_set.size(); dil_max++)
 		{
 			for(int dil_incr = 0; dil_incr<dilusion_increment_power.size(); dil_incr++)
 			{
-				dilusion_max = dilusion_max_set.at(dil_max);
-				dilusion_increment = dilusion_max/dilusion_increment_power.at(dil_incr);
+				sim_config.patience_max_val = dilusion_max_set.at(dil_max);
+				sim_config.patience_refill_inc = sim_config.patience_max_val/dilusion_increment_power.at(dil_incr);
 				oneExperiment(i);
 			}
 		}
@@ -197,14 +273,21 @@ void displaySimulation()
 {
 	setStaticVariables();
 	World world(Conf::WORLD_WIDTH, Conf::WORLD_HEIGHT);
-	Colony colony(Conf::COLONY_POSITION.x, Conf::COLONY_POSITION.y, Conf::ANTS_COUNT, 
-	malicious_fraction, malicious_timer_wait, malicious_ants_focus, ant_tracing_pattern, 
-	counter_pheromone, hell_phermn_intensity_multiplier);
+	Colony colony(Conf::COLONY_POSITION.x,
+				  Conf::COLONY_POSITION.y,
+				  Conf::ANTS_COUNT,
+				  sim_config.malicious_fraction,
+				  sim_config.malicious_timer_wait,
+				  sim_config.malicious_focus,
+				  sim_config.tracing_pattern,
+				  sim_config.patience_activation,
+				  sim_config.malicious_intensity_mult);
 
 	sf::ContextSettings settings;
 	settings.antialiasingLevel = 4;
 	initWorld(world, colony);
-	sf::RenderWindow window(sf::VideoMode(Conf::WIN_WIDTH, Conf::WIN_HEIGHT), "AntSim", sf::Style::Default, settings);
+	auto sf_gui_display_style = sim_config.gui_fullscreen ? sf::Style::Fullscreen : sf::Style::Default;
+	sf::RenderWindow window(sf::VideoMode(Conf::WIN_WIDTH, Conf::WIN_HEIGHT), "AntSim", sf_gui_display_style, settings);
 	window.setFramerateLimit(60);
 
 	DisplayManager display_manager(window, window, world, colony);
@@ -260,7 +343,7 @@ int main()
 	Conf::loadTextures();
 	
 	loadUserConf();
-	if(DISPLAY_GUI)
+	if(sim_config.gui_display)
 		displaySimulation();
 	else
 		simulateAnts();
